0d82197d8c4bb2d071ec4553d25eab5bfbcd49b5
gabrielguidini <gabrielsguidini@gmail.com>
2023-11-27 13:02:58 -0300

    first commit, adding multiple lines of code, files, structure and algorithm logic

---
+++ src/Application.java
@@ -0,0 +1,24 @@
+import list.transmiter.GameCreator;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Quanto tempo, em minutos, você quer para digitar as palavras?");
+        Long minutes = sc.nextLong();
+        GameCreator gameCreator = new GameCreator(minutes);
+        List<String> allWords = gameCreator.createLists();
+
+        System.out.println("Comece a digitar as palavras: ");
+        List<String> wordsWritten = new ArrayList<>();
+        do{
+            String word = sc.nextLine();
+            wordsWritten.add(word);
+            System.out.println(wordsWritten);
+        }while(gameCreator.countDownTimer() != 0);
+    }
+}
---
+++ src/list/transmiter/GameCreator.java
@@ -0,0 +1,50 @@
+package list.transmiter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class GameCreator {
+    private static Timer timer = new Timer();
+    private Long time;
+    private List<String> verifiedWords;
+
+    public GameCreator(Long time) {
+        this.time = time*60;
+    }
+    public List<String> createLists() throws FileNotFoundException {
+        File file = new File("/home/guidini/Desktop/bombas/palavras");
+        Scanner sc = new Scanner(file);
+        List<String> list = new ArrayList<>();
+        while(sc.hasNext()){
+            list.add(sc.nextLine());
+            if(list.size() > 20) {
+                break;
+            }
+        }
+        sc.close();
+        return list;
+    }
+    public long countDownTimer() {
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if(time>0){
+                    time--;
+                }else{
+                    System.out.println("Acabou o tempo!!");
+                    timer.cancel();
+                    System.exit(1);
+                }
+            }
+        },1000,1000);
+        return time;
+    }
+
+    public List<String> verifyWords(List<String> allWords, List<String> wordsWritten){
+
+
+        return verifiedWords;
+    }
+
+}
